fd773381ca2e13f4e5a875e398573a30b7683143
skyrus <skyrus@gmail.com>
2014-04-30 08:56:39 -0700

    starting binary search tree

---
+++ Random_BinarySearchTree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+class BinarySearchTree {
+  
+};
+
+int main() {
+  std::vector<int> inpSequence = { 10, 2, 3, 65, 3, 34, 5, 76, 3, 3, 2, 4, 6, 78, 8, 34, 3, 67 };
+
+  return 0;
+}
