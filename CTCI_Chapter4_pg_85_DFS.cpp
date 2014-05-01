5bc4eff589552468c67b78380aca5a4c1e088e55
skyrus <skyrus@gmail.com>
2014-05-01 08:20:27 -0700

    starting graph algorithms

--- CTCI_Chapter4_pg_85_DFS.cpp
+++ CTCI_Chapter4_pg_85_DFS.cpp
@@ -1,4 +1,63 @@
 #include <iostream>
+#include <set>
+#include <string>
 #include <vector>
+#include <cassert>
+
+// undirected graph
+class UdGraph {
+public:
+  std::vector<std::set<int> > adj;
+  UdGraph(int numVertices) { adj.resize(numVertices); };
+  void insertEdge(std::string edge);
+  void constructGraphFromEdges(std::string graph);
+  void printGraph();
+};
+
+void UdGraph::printGraph() {
+  for (auto it = adj.begin(); it!= adj.end(); ++it) {
+    std::cout << "node:" << std::distance(adj.begin(), it) << " neighbors:";
+    std::copy(it->begin(), it->end(), std::ostream_iterator<int>(std::cout, ","));
+    std::cout << "\n";
+  }
+}
+
+void UdGraph::constructGraphFromEdges(std::string graph) {
+  auto itMover = graph.begin();
+  while (itMover != graph.end()) {
+    auto openBraceIt = std::find(itMover, graph.end(), '(');
+    auto closeBraceIt = std::find(itMover, graph.end(), ')');
+    if (openBraceIt == graph.end() || closeBraceIt == graph.end()) { return; }
+    assert(openBraceIt + 1 != graph.end());
+    auto posOpenBracePlusOne = std::distance(graph.begin(), openBraceIt + 1);
+    auto len = std::distance(openBraceIt + 1, closeBraceIt);
+    insertEdge(graph.substr(posOpenBracePlusOne, len));
+    itMover = closeBraceIt + 1;
+  }
+}
+
+void UdGraph::insertEdge(std::string edge) {
+  assert(std::find(edge.begin(), edge.end(), ')') == edge.end());
+  assert(std::find(edge.begin(), edge.end(), '(') == edge.end());
+  auto itComma = std::find(edge.begin(), edge.end(), ',');
+  if (itComma == edge.end()) { return; }
+  std::string v1Temp(edge.begin(), itComma);
+  assert(itComma + 1 != edge.end());
+  std::string v2Temp(itComma + 1, edge.end());
+  auto v1 = std::stoi(v1Temp);
+  auto v2 = std::stoi(v2Temp);
+  assert(v1 < static_cast<int>(adj.size()));
+  assert(v2 < static_cast<int>(adj.size()));
+  adj[v1].insert(v2);
+  adj[v2].insert(v1);
+}
 
 
+int main() {
+  UdGraph g(10);
+  std::string inputGraph = {"(1,2),(1,3),(1,4),(4,5),(2,6),(6,5),(8,9),(3,8)"};
+  //std::cin >> inputGraph;
+  g.constructGraphFromEdges(inputGraph);
+  g.printGraph();
+  return 0;
+}
--- Random_BinarySearchTree.cpp
+++ Random_BinarySearchTree.cpp
@@ -1,15 +1,91 @@
+// to revise binary search tree
+// shared_ptr - passed by reference and const reference. In this example, we never need to copy shared_ptr because ownership is never shared
+// pre-order traversal of tree
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
-class BinarySearchTree {
-  
+class Node {
+public:
+  int key;
+  shared_ptr<Node> leftChild = nullptr;
+  shared_ptr<Node> rightChild = nullptr;
+  Node(const int keyArg) : key(keyArg) {}
 };
+
+class BinarySearchTree {
+public:
+  void insert(const int key);
+  bool contains(const int key);
+  void printPreOrder();
+private:
+  std::shared_ptr<Node> _root = nullptr;
+  void _insertRecursively(shared_ptr<Node>& rootOfSubTree, const int key);
+  bool _searchRecursively(const shared_ptr<Node>& rootOfSubTree, const int key);
+  void _printPreOrder(shared_ptr<Node>& rootOfSubTree);
+};
+
+
+void BinarySearchTree::insert(const int key) {
+  _insertRecursively(_root, key);
+}
+
+bool BinarySearchTree::contains(const int key) {
+  return _searchRecursively(_root, key);
+}
+
+void BinarySearchTree::printPreOrder() {
+  _printPreOrder(_root);
+  std::cout << "\n";
+}
+
+void BinarySearchTree::_printPreOrder(shared_ptr<Node>& rootOfSubTree) {
+  if (rootOfSubTree == nullptr) { return; }
+  std::cout << rootOfSubTree->key << ", ";
+  _printPreOrder(rootOfSubTree->leftChild);
+  _printPreOrder(rootOfSubTree->rightChild);
+}
+
+void BinarySearchTree::_insertRecursively(shared_ptr<Node>& rootOfSubTree, const int key) {
+  if (rootOfSubTree == nullptr) {
+    rootOfSubTree = std::make_shared<Node>(key);
+  }
+  if (key < rootOfSubTree->key) {
+    _insertRecursively(rootOfSubTree->leftChild, key);
+  }
+  else if (key > rootOfSubTree->key) {
+    _insertRecursively(rootOfSubTree->rightChild, key);
+  }
+}
+
+bool BinarySearchTree::_searchRecursively(const shared_ptr<Node>& rootOfSubTree, const int key) {
+  if (rootOfSubTree == nullptr) {
+    return false;
+  }
+  if (key < rootOfSubTree->key) {
+    return _searchRecursively(rootOfSubTree->leftChild, key);
+  }
+  else if (key > rootOfSubTree->key) {
+    return _searchRecursively(rootOfSubTree->rightChild, key);
+  }
+  else {
+    return true;
+  }
+}
 
 int main() {
   std::vector<int> inpSequence = { 10, 2, 3, 65, 3, 34, 5, 76, 3, 3, 2, 4, 6, 78, 8, 34, 3, 67 };
-
+  BinarySearchTree bst;
+  for (int i : inpSequence) {
+    bst.insert(i);
+    bst.printPreOrder();
+  }
+  for (int i : inpSequence) {
+    cout << bst.contains(i) << "\n";
+  }
+  cout << bst.contains(1000) << "\n";
   return 0;
 }
