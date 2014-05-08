f43d81ac8b0ea49d917ba04e5baf9a93dbd93c83
skyrus <skyrus@gmail.com>
2014-05-08 11:04:25 -0700

    updateToBitManipulation

--- CTCI_pg91_BitManip.cpp
+++ CTCI_pg91_BitManip.cpp
@@ -1,5 +1,7 @@
 // using std::bitset
 // remember: no shortcut operators for bitwise operators
+// findNextGreaterNumberWithSameNumberOfOnes, SWAP LSB first one with a first zero after 1, and push remaining ones before prev first one towards LSB as xyz...1111..111 (example - 110 0 11 -> 110 1 10 -> 110 1 01) 
+// findPrevSmallerNumberWithSameNumberOfOnes, SWAP LSB first zero with first one after the 0, and push the remaining ones towards the old one's posistion , ie. ,xyz...01111 (example - 1 1 0 0 11 --> 1 0 0 1 11 --> 1 0 1110)
 #include <iostream>
 #include <iterator>
 #include <bitset>
@@ -7,23 +9,22 @@
 #include <ctime>
 #include <cstdlib>
 
-void insertNToMAtGivenPos(const int M, int& N, const int i, const int j) {
+void insertNToMAtGivenPos(const unsigned int M, unsigned int& N, const unsigned int i, const unsigned int j) {
   assert( N >= ((1 << (j +1  - i))<< i) );
   N = N & (~(1 << (j + 1 - i))) << i;
   N = N | (M << i);
 }
 
-int findGreaterNumberWithSameNumberOfOnes(int N) {
+unsigned int findNextGreaterNumberWithSameNumberOfOnes(unsigned int N) {
   assert(N > 0);
-  N = 12;
-  int index = 0;
-  int nextN = N;
-  int lowest1Pos = -1;
-  int lowest0After1Pos = -1;
+  unsigned int index = 0;
+  unsigned int nextN = N;
+  unsigned int lowest1Pos = -1;
+  unsigned int lowest0After1Pos = -1;
   while (1) {
-    int temp = N & (1 << index);
+    unsigned int temp = N & (1 << index);
     if (lowest1Pos == -1) {
-      if (temp > 1) { lowest1Pos = index; }
+      if (temp > 0) { lowest1Pos = index; }
     }
     else if (lowest0After1Pos == -1 ) { 
       if (temp == 0) { lowest0After1Pos = index; }
@@ -35,7 +36,11 @@ int findGreaterNumberWithSameNumberOfOnes(int N) {
   }
   nextN = nextN & ~(1 << lowest1Pos);
   nextN = nextN | (1 << lowest0After1Pos);
-  int testNextN = N + 1;
+  const unsigned int lsbLowerThanSwapped1Pos = nextN & ((1 << lowest0After1Pos) - 1);
+  nextN = nextN & (~((1 << lowest0After1Pos) - 1));
+  nextN = nextN | ((1 << std::bitset<32>(lsbLowerThanSwapped1Pos).count()) - 1);
+
+  unsigned int testNextN = N;
   while (std::bitset<32>(++testNextN).count() != std::bitset<32>(N).count());
   std::cout << std::bitset<16>(N) << ", " << std::bitset<16>(nextN) << ", bruteForce" << std::bitset<16>(testNextN) << "\n";
   std::cout << N << ", " << nextN << ", bruteForce" << testNextN << "\n";
@@ -43,17 +48,51 @@ int findGreaterNumberWithSameNumberOfOnes(int N) {
   return nextN;
 }
 
+unsigned int findPrevSmallerNumberWithSameNumberOfOnes(unsigned int N) {
+  assert(N > 0);
+  unsigned int index = 0;
+  unsigned int prevN = N;
+  unsigned int lowest0Pos = -1;
+  unsigned int lowest1After0Pos = -1;
+  while (1) {
+    unsigned int temp = N & (1 << index);
+    if (lowest0Pos == -1) {
+      if (temp == 0) { lowest0Pos = index; }
+    }
+    else if (lowest1After0Pos == -1) {
+      if (temp > 0) { lowest1After0Pos = index; }
+    }
+    else  {
+      break;
+    }
+    ++index;
+  }
+  prevN = prevN | (1 << lowest0Pos);
+  prevN = prevN & (~(1 << lowest1After0Pos));
+  const unsigned int lowerThanOldSwapped1Pos = prevN & ((1 << lowest1After0Pos) - 1);
+  prevN = prevN & ~((1 << lowest1After0Pos) - 1);
+  const unsigned int numOnes = std::bitset<32>(lowerThanOldSwapped1Pos).count();
+  prevN = prevN | (((1 << numOnes) - 1) << (lowest1After0Pos - numOnes));
+  unsigned int testPrevN = N;
+  while (std::bitset<32>(--testPrevN).count() != std::bitset<32>(N).count());
+  std::cout << std::bitset<16>(N) << ", " << std::bitset<16>(prevN) << ", bruteForce:" << std::bitset<16>(testPrevN) << "\n";
+  std::cout << N << ", " << prevN << ", bruteForce:" << testPrevN << "\n";
+  assert(prevN == testPrevN);
+  return prevN;
+}
 
-int main() {
-  int N = 0x600;
-  int M = 0x13;
+
+unsigned int main() {
+  unsigned int N = 0x600;
+  unsigned int M = 0x13;
   std::cout << "M = " << std::bitset<16>(M) << ", N = " << std::bitset<16>(N) << std::endl;
   insertNToMAtGivenPos(M, N, 2, 6);
   std::cout << "M = " << std::bitset<16>(M) << ", N = " << std::bitset<16>(N) << std::endl;
   srand(std::time(0));
-  for (int i = 0; i < 100; ++i) {
-    int NN = std::rand();
-    int NN_next = findGreaterNumberWithSameNumberOfOnes(NN);
+  for (unsigned int i = 0; i < 100; ++i) {
+    unsigned int NN = std::rand();
+    unsigned int NN_next = findNextGreaterNumberWithSameNumberOfOnes(NN);
+    unsigned int NN_prev = findPrevSmallerNumberWithSameNumberOfOnes(NN);
   }
   return 0;
 }
